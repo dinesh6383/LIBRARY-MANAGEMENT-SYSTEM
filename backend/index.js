fd134195f105af5af1ff785dc993c251c6cd6e2b
dinesh6383 <dineshravi6383@gmail.com>
2023-03-08 19:24:05 +0530

    Initial commit

--- backend/index.js
+++ backend/index.js
@@ -35,7 +35,7 @@ app.use((err, req, res, next) => {
   });
 });
 
-const PORT = 8000;
+const PORT = process.env.PORT || 8000;
 app.listen(PORT, () => {
   mongo();
   console.log(`Port successfully started at ${PORT}`);
